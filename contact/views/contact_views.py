03dc31eee820720670831bf3826c851fd43f686a
Lucas <lpa4@aluno.ifal.edu.br>
2025-04-06 15:09:45 -0300

    querysets,criando a tabela dos contatos e organizando a view

---
+++ contact/views/contact_views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from contact.models import Contact
+# Create your views here.
+
+def index(request):
+    contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
+
+    print(contacts.query)
+
+    context = {
+        'contacts' : contacts,
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
