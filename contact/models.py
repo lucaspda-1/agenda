2ab326985c7eaa609d9ca1e1faf6b578ec71f207
Lucas <lpa4@aluno.ifal.edu.br>
2025-04-10 14:33:22 -0300

    usando instace do modelform

--- contact/models.py
+++ contact/models.py
@@ -28,17 +28,17 @@ class Contact(models.Model):
         verbose_name = 'Contato'
 
     first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True,verbose_name='Sobrenome')
     phone = models.CharField(max_length=50, verbose_name='Telefone')
     email = models.EmailField(max_length=254,blank=True)
     created_date = models.DateTimeField(default=timezone.now)
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True,verbose_name='Descrição')
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True , upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
-        blank=True, null=True) #tome cuidado ao usar CASCADE 
+        blank=True, null=True,verbose_name='Categoria') #tome cuidado ao usar CASCADE 
     owner = models.ForeignKey(
         User,
         on_delete=models.SET_NULL,
--- contact/urls.py
+++ contact/urls.py
@@ -7,6 +7,8 @@ app_name = 'contact'
 urlpatterns = [
     # contact (CRUD)
     path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
+
     path('contact/create/', views.create, name='create'),
 
     path('search/', views.search, name='search'),  
